dd65e250bd93e84472456ba142fc8465f7f60ecd
Ashutosh Mishra <93077081+ashutosh1808@users.noreply.github.com>
2023-10-06 00:39:27 +0530

    Update Tables.py

--- Program's_Contributed_By_Contributors/Python_Programs/Tables.py
+++ Program's_Contributed_By_Contributors/Python_Programs/Tables.py
@@ -3,6 +3,6 @@ wapp to print the table of a no
 '''
 
 num=float(input("enter a number: "))
-mul=int(input("enter last multiple: "))
+mul=int(input("enter last multiple: "))  
 for i in range(1,mul+1):
 	print(i,"*",num," = ",(i*num))
